bfd4031a53a04fced41c6893b68147db081bd582
Matt Morris <mcm0027@yahoo.com>
2015-10-07 10:45:41 -0500

    add <br>s

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
+$(document).ready(function() {
 for (var i = 1; i <=100; i++) {
 	if (i % 3 === 0 && i % 5 === 0) {
-		$("body").append("FizzBuzz  ");
+		$("body").append("FizzBuzz <br><br>");
 	} else if (i % 3 === 0) {
-		$("body").append("Fizz  ");
+		$("body").append("Fizz <br><br>");
 	} else if (i % 5 === 0) {
-		$("body").append("Buzz  ");
+		$("body").append("Buzz <br><br>");
 	} else {
-		$("body").append(i + "  ");
+		$("body").append(i + "<br><br>");
 	}
 }
+});
